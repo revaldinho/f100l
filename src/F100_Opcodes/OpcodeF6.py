48a7f7b9ca5385076b19daf8f876a07660b6edaa
revaldinho <richevans@mac.com>
2016-12-14 22:18:31 +0000

    Correct operand register setting in SBS

--- src/F100_Opcodes/OpcodeF6.py
+++ src/F100_Opcodes/OpcodeF6.py
@@ -2,30 +2,30 @@
 SBS - Subtract (With Carry), Store to Memory
 ============================================
 
-Perform substraction of the accumulator from the value of an operand with the 
+Perform substraction of the accumulator from the value of an operand with the
 result being stored in the operand's original location.
 
-When the multi-length 'M' flag is set, the carry bit from a previous operation is 
+When the multi-length 'M' flag is set, the carry bit from a previous operation is
 included in the subtraction.
 
 
 **Function**
 
-When M is clear, perform a simple subtraction 
+When M is clear, perform a simple subtraction
 
 ::
-   
-   SBS N      (N) <- (N) - A 
-   SBS ,D     (PC+1) <- D - A 
-   SBS /P     (P) <- (P) - A 
-   SBS /P+    P <- P + 1 ; (P) <- (P) - A 
+
+   SBS N      (N) <- (N) - A
+   SBS ,D     (PC+1) <- D - A
+   SBS /P     (P) <- (P) - A
+   SBS /P+    P <- P + 1 ; (P) <- (P) - A
    SBS /P-    (P) <- (P) - A ; P <- P - 1
-   SBS .W     (W) <- (W) - A 
+   SBS .W     (W) <- (W) - A
 
 When M is set, include the carry from a previous operation in the subtraction
 
 ::
-   
+
    SBS N      (N) <- (N) - A + C - 1
    SBS ,D     (PC+1) <- D - A + C - 1
    SBS /P     (P) <- (P) - A + C - 1
@@ -46,31 +46,31 @@ after the opcode, and the the result is returned to that location.
 | F  |I| | R| P               |                  |          |                      |
 +----+-+-+--+-----------------+-+----------------+----------+----------------------+
 |0110|0|  11'b<non-zero addr> |        none      | SBS N    | TBC                  |
-+----+-+-+-----+--------------+-+----------------+----------+----------------------+ 
++----+-+-+-----+--------------+-+----------------+----------+----------------------+
 |0110|0|  11'b000000000000    |     16'b<data>   | SBS ,D   | TBC                  |
 +----+-+-+--+-----------------+-+----------------+----------+----------------------+
 |0110|1|x|x0|8'b<non-zero ptr>|        none      | SBS /P   | TBC                  |
-+----+-+-+--+-----------------+-+----------------+----------+----------------------+ 
++----+-+-+--+-----------------+-+----------------+----------+----------------------+
 |0110|1|x|01|8'b<non-zero ptr>|        none      | SBS /P+  | TBC                  |
-+----+-+-+--+-----------------+-+----------------+----------+----------------------+  
++----+-+-+--+-----------------+-+----------------+----------+----------------------+
 |0110|1|x|11|8'b<non-zero ptr>|        none      | SBS /P-  | TBC                  |
-+----+-+-+--+-----------------+-+----------------+----------+----------------------+ 
++----+-+-+--+-----------------+-+----------------+----------+----------------------+
 |0110|1|x|xx|8'b00000000      |x|   15'b<addr>   | SBS .W   | TBC                  |
 +----+-+-+--+-----------------+-+----------------+----------+----------------------+
 
- 
+
 **Condition Register**
 
 +---+---+---+---+---+---+---+
 | F | M | C | S | V | Z | I |
 +---+---+---+---+---+---+---+
-|\--|\--| * | * | * | * |\--| 
-+---+---+---+---+---+---+---+ 
+|\--|\--| * | * | * | * |\--|
++---+---+---+---+---+---+---+
 
 * C is cleared if the operation results in a borrow (carry), otherwise set
 * Z is set if the result is all-zeroes, otherwise cleared
 * S is set if the MSB of the result is a '1', otherwise cleared
-* V is set if the subtraction of two numbers of different sign results in 
+* V is set if the subtraction of two numbers of different sign results in
   a number with the same sign as the subtrahend, otherwise cleared
 
 
@@ -79,22 +79,22 @@ after the opcode, and the the result is returned to that location.
 from .F100_Opcode import *
 
 class OpcodeF6(F100_Opcode) :
-    
+
     def __init__ (self, CPU=None):
         super().__init__( opcode_fn = { "SBS":6}, CPU=CPU )
         self.F = 6
 
     def execute(self):
         cycle_count = 0
-        
-        (operand, operand_address, cycles) = self.get_operand()
-        
+
+        (self.CPU.OR, operand_address, cycles) = self.get_operand()
+
         result = self.CPU.OR - self.CPU.ACC
         if (self.CPU.CR.M==1) :
             result = result + self.CPU.CR.C - 1
 
-        self.CPU.memory_write(operand_address, result)     
-            
+        self.CPU.memory_write(operand_address, result)
+
         self.CPU.CR.C = 0 if (result & 0x010000) > 0 else 1
         self.CPU.CR.Z = 1 if (result & 0xFFFF) == 0 else 0
         self.CPU.CR.S = 1 if (result & 0x8000) != 0 else 0
@@ -102,6 +102,5 @@ class OpcodeF6(F100_Opcode) :
             self.CPU.CR.V = 1
         else:
             self.CPU.CR.V = 0
-    
-        return cycle_count        
 
+        return cycle_count
